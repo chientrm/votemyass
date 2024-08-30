dc76eddd2bdbbb33a2a2427781201cadc9c733a5
Chien Tran <chientrm@gmail.com>
2024-08-30 14:02:14 +0700

    f

--- src/routes/test/+server.ts
+++ src/routes/test/+server.ts
@@ -1,4 +1,6 @@
-export const load = ({ request }) => {
+import { json } from '@sveltejs/kit';
+
+export const GET = ({ request }) => {
 	const tmp = request.headers.get('cf-connecting-ip');
-	return { tmp };
+	return json({ tmp });
 };
