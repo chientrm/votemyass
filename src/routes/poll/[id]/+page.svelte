c6b42f79c0fd9052b0a31340e0687a75d833f767
Chien Tran <chientrm@gmail.com>
2024-08-31 00:23:05 +0700

    f

--- src/routes/poll/[id]/+page.svelte
+++ src/routes/poll/[id]/+page.svelte
@@ -2,13 +2,21 @@
 	import { enhance } from '$app/forms';
 	import { Button } from '$lib/components/ui/button';
 	import * as PageHeader from '$lib/components/ui/page-header';
+	import { cn } from '$lib/utils';
 	export let data;
 	$: myVoteResult = data.voteResults.find((vote) => vote.userId === data.userId);
+	$: yes = data.pollResults[0].yes;
+	$: votes = data.pollResults[0].votes;
 </script>
 
 <PageHeader.Root>
 	<PageHeader.Heading>{data.pollResults[0].title}</PageHeader.Heading>
-	<PageHeader.Description balanced={false}>{data.voteResults.length} votes</PageHeader.Description>
+	<PageHeader.Description
+		balanced={false}
+		class={cn('p-2 text-white', yes / votes >= 0.5 ? 'bg-green-500' : 'bg-red-500')}
+	>
+		{yes} yes/ {votes} votes ({((100 * yes) / votes).toFixed(0)} %)
+	</PageHeader.Description>
 </PageHeader.Root>
 
 {#if myVoteResult}
